26d8bb9866b3d090333dc010afead618d8fa6bc0
Ivan Peevski <cyberhorse@users.sourceforge.net>
2006-03-04 23:51:59 +0000

    updated lists

--- money/config.php
+++ money/config.php
@@ -1,8 +1,8 @@
 <?php
 $cfg['db'] = 'money';
 $cfg['userdb'] = $cfg['db'];
-$cfg['user'] = 'root';
-$cfg['pass'] = 'k0H4e65';
+$cfg['user'] = '';
+$cfg['pass'] = '';
 
 $cfg['text_fields'] = array('person', 'category', 'notes', 'title', 'desc');
 
