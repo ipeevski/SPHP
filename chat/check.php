7bb3d07429d498c45f201e85c34275790d52b9ba
Ivan Peevski <cyberhorse@users.sf.net>
2017-03-31 00:40:24 +1030

    Fix syntax issue

--- chat/check.php
+++ chat/check.php
@@ -6,11 +6,11 @@
 <?php
 	include "functions.php";
     
-	if (!empty($_POST[red])) {
+	if (!empty($_POST['red'])) {
 		echo "Yay, you found the treasure :). 
-	}
 The treasure is a list of all emoticons on here! pretty cool eh? <br>
- <a href=\"emoticons.php\">Click!</a> "
+ <a href=\"emoticons.php\">Click!</a> ";
+	}
 ?>
 		<form action="check.php" method="post">
 			<input type="submit" name="red" value="Big Red Button">
